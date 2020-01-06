c8fc68cee3656bea3d1102e84ab16adfb89c0df6
Mattie Zilaie <your_name@domain.comAccount>
2020-01-06 15:23:54 -0800

    Class called person with data members(name and age) and function outside of class called basic_stats has a loop that adds all ages of persons in person_list and then uses the stats library to return the mean. median, and mode in the form of a tuple

--- basic_stats.py
+++ basic_stats.py
@@ -27,10 +27,4 @@ def basic_stats(person_list):
     mode_age = mode(age)
     return mean_age, median_age, mode_age
 
-p1 = Person("Kyoungmin", 73)
-p2 = Person("Mercedes", 24)
-p3 = Person("Avanika", 48)
-p4 = Person("Marta", 24)
 
-person_list = [p1, p2, p3, p4]
-print(basic_stats(person_list)) # should print a tuple of three values
